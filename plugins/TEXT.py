03f7348eb42e67e1dbd56ddc0fe491cd23e432d3
HappyBoy0509 <65845452+HappyBoy0509@users.noreply.github.com>
2020-07-08 22:18:28 +0530

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -1,11 +1,11 @@
 # Here is Your Drive Folder Name You can Replace it with Your Desire name(Optional)
-drive_folder_name = "U2GD_bot"
+drive_folder_name = "HB4All"
 
 # Enter Your Mega email And Pass (Required)
 MEGA_EMAIL = "email"
 MEGA_PASSWORD = "password"
 
-START = "Hi {}  \nI am Upload 2 Drive Bot.\nPlease Authorise To use me.\nBy using /auth \n\nFor more info /help\n\nPlease Report Bugs @aryanvikash"
+START = "Hi {}  \nI Am Google Drive Uploader Robot.\nPlease Authorise To use me.\nBy using /auth \n\nFor more info /help\n\nSupport HB4All"
 
 HELP = """<b>AUTHORISE BOT</b>
 Use  /auth Command Generate
@@ -19,8 +19,8 @@ command.\n\n
 You Can Upload Any Internet
 Files On Your google
 Drive Account.\n\n
-This Bot is by @Bhadoo\n
-Bug Report @aryanvikash"""
+This Bot is by HB4All\n
+Support HB4All @HB4All_Bot"""
 
 DP_DOWNLOAD = "Dropbox Link !! Downloading Started ..."
 OL_DOWNLOAD = "Openload Link !! Downloading Started ... \nOpenload Links Are Extremely Slow"
@@ -30,19 +30,19 @@ DOWNLOAD = "Downloading Started ..."
 DOWN_MEGA = "Downloading Started... \nMega Links are \nExtremely Slow :("
 DOWN_COMPLETE = "Downloading complete!!"
 NOT_AUTH = "You Are Not Authorised To Using this Bot \n\nPlease Authorise Me Using /auth"
-REVOKE_FAIL = "You Are Already UnAuthorised. \n\nPlease Use /auth To Authorise. \n\nreport At @aryanvikash "
-AUTH_SUCC = "Authorised Successfully  !! \n\nNow Send me A direct Link :)"
-ALREADY_AUTH = "You Are Already Authorised!\n\nWanna Change Drive Account? \n\nUse /revoke \n\nreport At @aryanvikash "
+REVOKE_FAIL = "You Are Already UnAuthorised. \n\nPlease Use /auth To Authorise. \n\nreport At @HB4All1_Bot "
+AUTH_SUCC = "Authorised Successfully  !! \n\nNow Send Me A Direct Link :)"
+ALREADY_AUTH = "You Are Already Authorised!\n\nWanna Change Drive Account? \n\nUse /revoke \n\nReport At @HB4All1_Bot "
 AUTH_URL = '<a href ="{}">Click Here to Get Auth Code</a>\n\nGenerate And Copy Your Google Drive Token And Send It To Me'
 UPLOADING = "Download Complet !! \nUploading Your file"
 REVOKE_TOK = " Your Token is Revoked Successfully!! \n\nUse /auth To Re-Authorise Your Drive Acc."
 # DOWN_PATH = "Downloads\\" #windows path
 DOWN_PATH = "Downloads/"  # Linux path
 DOWNLOAD_URL = "Your File Uploaded Successfully.\n\n <b>Filename</b> : {} \n\n <b> Size</b> : {} MB \n\n <b>Download</b> {}"
-AUTH_ERROR = "AUTH Error!! Please Send Me a valid Token or Re-Authorise Me\n\n/auth\n\n report At @aryanvikash"
+AUTH_ERROR = "AUTH Error!! Please Send Me a valid Token or Re-Authorise Me\n\n/auth\n\"
 OPENLOAD = True
 DROPBOX = True
 MEGA = True
 
 
-UPDATE = """Try @BhadooCloud"""
+UPDATE = """Nope Contact HB4All @HB4All1_Bot"""
