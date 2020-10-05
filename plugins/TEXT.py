7a721e23b7f1fe03169b8d79ffee93d769903662
Munalia <69430348+Munalia@users.noreply.github.com>
2020-10-05 13:53:03 +0530

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -1,11 +1,11 @@
 # Here is Your Drive Folder Name You can Replace it with Your Desire name(Optional)
-drive_folder_name = "HB4All"
+drive_folder_name = "Uploader Bot"
 
 # Enter Your Mega email And Pass (Required)
 MEGA_EMAIL = "email"
 MEGA_PASSWORD = "password"
 
-START = "Hi {}  \nI Am Google Drive Uploader Robot.\nPlease Authorise To Use Me.\nBy Using /auth For More Info /help\n\nSupport HB4All"
+START = "Hi {}  \nI Am Google Drive Uploader Robot.\nPlease Authorise To Use Me.\nBy Using /auth For More Info /help"
 
 HELP = """<b>AUTHORISE BOT</b>
 Use  /auth Command Generate
@@ -16,9 +16,7 @@ How To Use https://telegra.ph/How-To-Use-Gdrive-Uploader-Bot-07-26
 Account ?</b>
 You can use /revoke command.\n\n
 <b>What I Can Do With This Bot?</b>
-You Can Upload Direct Link To Your google Drive Account.\n\n
-This Bot is by HB4All\n
-Support HB4All @HB4All_Bot"""
+You Can Upload Direct Link To Your google Drive Account."""
 
 DP_DOWNLOAD = "Dropbox Link !! Downloading Started ..."
 OL_DOWNLOAD = "Openload Link !! Downloading Started ... \nOpenload Links Are Extremely Slow"
@@ -28,9 +26,9 @@ DOWNLOAD = "Downloading Started ..."
 DOWN_MEGA = "Downloading Started... \nMega Links are \nExtremely Slow :("
 DOWN_COMPLETE = "Downloading complete!!"
 NOT_AUTH = "You Are Not Authorised To Using this Bot \n\nPlease Authorise Me Using /auth"
-REVOKE_FAIL = "You Are Already UnAuthorised. \n\nPlease Use /auth To Authorise. \n\nreport At @HB4All1_Bot "
+REVOKE_FAIL = "You Are Already UnAuthorised. \n\nPlease Use /auth To Authorise."
 AUTH_SUCC = "Authorised Successfully  !! \n\nNow Send Me A Direct Link :)"
-ALREADY_AUTH = "You Are Already Authorised!\n\nWanna Change Drive Account? \n\nUse /revoke \n\nReport At @HB4All1_Bot "
+ALREADY_AUTH = "You Are Already Authorised!\n\nWanna Change Drive Account? \n\nUse /revoke"
 AUTH_URL = '<a href ="{}">Click Here to Get Auth Code</a>\n\nGenerate And Copy Your Google Drive Token And Send It To Me\n\nFollow The Steps Below\nhttps://telegra.ph/How-To-Use-Gdrive-Uploader-Bot-07-26'
 UPLOADING = "Download Complet !! \nUploading Your file"
 REVOKE_TOK = " Your Token is Revoked Successfully!! \n\nUse /auth To Re-Authorise Your Drive Acc."
@@ -43,4 +41,4 @@ DROPBOX = True
 MEGA = True
 
 
-UPDATE = """Nope Contact HB4All @HB4All1_Bot"""
+UPDATE = """Nope, Now Nothing Any Updates"""
